28277edd237e52645947e3a2140a843107fbec79
Sravanthi <sravanthidasari@hotmail.com>
2020-03-04 00:48:15 -0800

    Adding logic and styles

--- controllers/grocery.js
+++ controllers/grocery.js
@@ -1,68 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Grocery = require('../models/groceries.js');
+const Grocery = require("../models/groceries.js");
 
-
-//new
-router.get('/new', (req,res) => {
-  res.render('groceries/new.ejs');
+// new
+router.get("/new", (req, res) => {
+  res.render("groceries/new.ejs");
 });
 
-//index
-router.get('/', (req,res) => {
-  Grocery.find({},(err, allGroceries) => {
-      res.render('groceries/index.ejs', {
-          groceries: allGroceries
-      });
-  });
-});
-
-//create
-router.post('/', (req,res) => {
-  if(req.body.itemPriority >= 1){
-      req.body.itemPriority = "normal";
-  } else {
-      req.body.itemPriority = "high";
-  }
-
-  Grocery.create(req.body, (err, newGroceryItem) => {
-      res.redirect("/groceries");
-  });
-});
-
-//show
-router.get('/:groceryItem', (req,res) => {
-  Grocery.findOne({groceryItem: req.params.groceryItem}, (err, allGroceries) => {
-      res.render('groceries/show.ejs', {
-          groceries: allGroceries
-      });
-  });
-});
-
-//Edit
-router.get('/:groceryItem/edit', (req,res) => {
-  Grocery.findOne({groceryItem: req.params.groceryItem}, (err,allGroceries) =>{
-      res.render('groceries/edit.ejs', {
-          groceries: allGroceries
-      });
-  });
-}),
-
-//Update
-router.put('/:groceryItem', (req,res) => {
-  const newItem = req.body;
-  newItem. itemPriority = newItem. itemPriority >= 1? normal : high;
-  Log.findOneAndUpdate({ groceryItem: req.params.groceryItem }, req.body, { new: true }, (err, data) => {
-    res.render('groceries/show.ejs', {
+// index
+router.get("/", (req, res) => {
+  Grocery.find({}, (err, allGroceries) => {
+    res.render("groceries/index.ejs", {
       groceries: allGroceries
     });
-});
+  });
 });
 
-//Delete
-router.delete('/:groceryItem', (req, res) => {
-  Log.findOneAndDelete({ groceryItem: req.params.groceryItem }, (err, data) => {
-      res.redirect("/groceries");
+// create
+router.post("/", (req, res) => {
+  const newItem = req.body;
+  newItem.itemPriority = "high";
+  newItem.totalQuantity = 0;
+
+  Grocery.create(newItem, (err, newGroceryItem) => {
+    res.redirect("/groceries");
+  });
+});
+
+// show
+router.get("/:groceryItem", (req, res) => {
+  Grocery.findOne({ groceryItem: req.params.groceryItem }, (err, allGroceries) => {
+    res.render("groceries/show.ejs", {
+      groceries: allGroceries
+    });
+  });
+});
+
+// edit
+router.get("/:groceryItem/edit", (req, res) => {
+  Grocery.findOne({ groceryItem: req.params.groceryItem }, (err, allGroceries) => {
+    res.render("groceries/edit.ejs", {
+      groceries: allGroceries
+    });
+  });
+});
+
+// update
+router.put("/:groceryItem", (req, res) => {
+  Grocery.findOne({ groceryItem: req.params.groceryItem }, (err, data) => {
+    const newItem = data;
+
+    if (req.body.restock !== undefined) {
+      newItem.totalQuantity += newItem.restockQuantity;
+    } else if (req.body.consume !== undefined) {
+      newItem.totalQuantity -= newItem.consumeQuantity;
+      if (newItem.totalQuantity < 0) {
+        newItem.totalQuantity = 0;
+      }
+    } else {
+      newItem.restockQuantity = parseInt(req.body.restockQuantity, 10) || 1;
+      newItem.consumeQuantity = parseInt(req.body.consumeQuantity, 10) || 1;
+      newItem.totalQuantity = parseInt(req.body.totalQuantity, 10);
+    }
+
+    if (newItem.totalQuantity <= newItem.consumeQuantity) {
+      newItem.itemPriority = "high";
+    } else {
+      newItem.itemPriority = "normal";
+    }
+
+    Grocery.findOneAndUpdate({ groceryItem: req.params.groceryItem }, newItem, { new: true }, (err, updatedItem) => {
+      res.render("groceries/show.ejs", {
+        groceries: updatedItem
+      });
+    });
+  });
+});
+
+// delete
+router.delete("/:groceryItem", (req, res) => {
+  Grocery.findOneAndDelete({ groceryItem: req.params.groceryItem }, (err, allGroceries) => {
+    res.redirect("/groceries");
   });
 });
 
--- models/groceries.js
+++ models/groceries.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose");
 
 const grocerySchema = new mongoose.Schema(
   {
-    groceryItem: { type: String, required: true },
+    groceryItem: { type: String, required: true, unique: true },
     itemPriority: String,
     restockQuantity: { type: Number, required: true },
-    consumeQuantity: { type: Number, required: true }
+    consumeQuantity: { type: Number, required: true },
+    totalQuantity: { type: Number, required: true }
   },
   {
     timestamps: true
